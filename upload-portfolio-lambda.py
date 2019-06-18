6c275c51c1a2ab8b67041ccf9a2f112219d06ebe
Colum Thorne <columthorne@gmail.com>
2019-06-18 09:18:52 +0100

    Update lambda function - fix some case sensitivity issues

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -19,8 +19,8 @@ def lambda_handler(event, context):
 
         if job:
             for artifact in job["data"]["inputArtifacts"]:
-                if artifact["name"] = "MyAppBuild":
-                    location = artifact["location"]["s3location"]
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
 
         print "Building portfolio from " + str(location)
         s3 = boto3.resource('s3')
@@ -38,6 +38,7 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully")
+
         if job:
             codepipeline = boto3.client('codepipeline')
             codepipeline.put_job_success_result(jobId=job["id"])
